48d7c081d36b42db65c3e173302d5703747d5776
RahulBurugoju <rahul9399555@gmail.com>
2026-07-20 18:24:15 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
-// require('dotenv').config({path:'./env'})
-
 
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
-
 import dns from 'dns'
+import { app } from './app.js'
+
+
 dns.setServers(['1.1.1.1',"8.8.8.8"])
 
 dotenv.config({path:'./.env'})
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running on port : ${process.env.PORT}`);
+    })
+})
+.catch((error)=>{
+    console.log("MongoDB connection failed :" , error);
+    
+});
    
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+
+const asyncHandler = (requestHandler)=>{
+    return (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
+    }
+}
+
+export {asyncHandler}
+// function asyncHandler(fn){
+//     return async function (req,res,next) {
+        
+//         try{
+//             await fn(req,res,next)
+//         }catch(error){
+//             res.status(error.code || 500).json({
+//                 success:false,
+//                 message:error.message
+//             })    
+//         }
+//     }
+// }
+
+// const asyncHandler = (fn)=> (req,res,next)=>{
+//     try{
+//         await fn(req,res,next)
+//     }catch(error){
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:error.message
+//         })    
+//     }
+// }
